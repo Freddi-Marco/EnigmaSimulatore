89ca429035b2340d509a4b619482f6975cc06c34
Faris <fa3185701@gmail.com>
2025-04-01 11:29:32 +0200

    Aggiunto PannelloScambiatore e Cavi

--- src/main/java/com/iiscastelli/enigmasimulatore/EnigmaController.java
+++ src/main/java/com/iiscastelli/enigmasimulatore/EnigmaController.java
@@ -17,14 +17,17 @@ import javafx.scene.text.TextAlignment;
 public class EnigmaController {
     public HBox hBoxRot;
     public HBox hBoxPos;
+    public GridPane gridCavi;
     @FXML
     private GridPane gridLuci;
     @FXML
     private GridPane gridTastiera;
     private Button[] buttons;
     private Circle[] luci;
+    private Circle[] cavi;
     private Label[] posLabels;
     static private GestioneRotori gr;
+    static private PannelloScambiatore ps = new PannelloScambiatore();
 
     @FXML
     private TextArea textInputArea;
@@ -41,6 +44,19 @@ public class EnigmaController {
         textInputArea.setText(inputText.toString());
         textOutputArea.setText(outputText.toString());
     }
+
+    public static Color getCavoColor(int index) {
+        switch (index) {
+            case 1: return Color.BLUE;
+            case 2: return Color.GREEN;
+            case 3: return Color.ORANGE;
+            case 4: return Color.PURPLE;
+            case 5: return Color.YELLOW;
+            case 6: return Color.BROWN;
+            default: return Color.BLACK; // Colore di default per valori non validi
+        }
+    }
+
 
     private void aggiornaLabelPosRotori() {
         for (int i = 0; i < posLabels.length; i++) {
@@ -137,6 +153,52 @@ public class EnigmaController {
         }
     }
 
+
+
+    private void inizializzaCavi() {
+        cavi = new Circle[27];
+        char lettera = 'A';
+        gridLuci.setVgap(5);
+        gridLuci.setHgap(10);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                cavi[i * 9 + j] = new Circle();
+                cavi[i * 9 + j].setFill(Paint.valueOf("#222222"));
+                cavi[i * 9 + j].setRadius(15);
+                cavi[i * 9 + j].setStroke(Color.WHITE);
+                cavi[i * 9 + j].setStrokeWidth(1);
+
+                final char letteraF = lettera;
+
+                cavi[i * 9 + j].setOnMouseClicked(e -> {
+                    ps.select(letteraF);
+                    aggiornaCavi();
+                });
+
+                Label letteraLabel = new Label(String.valueOf(letteraF));
+                letteraLabel.setPrefWidth(300 / 10);
+                letteraLabel.setPadding(new Insets(0, 0, 10, 0));
+                letteraLabel.setTextAlignment(TextAlignment.CENTER);
+                letteraLabel.setAlignment(Pos.CENTER);
+                letteraLabel.setTextFill(Color.WHITE);
+                gridCavi.add(letteraLabel, j, (i * 2) + 1);
+
+                gridCavi.add(cavi[i * 9 + j], j, i * 2);
+
+                lettera++;
+                if (lettera == '[') return;
+            }
+        }
+    }
+
+    private void aggiornaCavi() {
+        int[] layout = ps.ritornaLayout();
+
+        for (int i = 0; i < layout.length; i++) {
+            cavi[i].setFill(getCavoColor(layout[i]));
+        }
+    }
+
     private void inizializzaTastiera() {
         buttons = new Button[27];
         char lettera = 'A';
@@ -149,7 +211,7 @@ public class EnigmaController {
                 final char letteraF = lettera;
                 buttons[i * 9 + j].setOnAction(e -> {
                     if (gr != null) {
-                        final char codificato = gr.codifica(letteraF);
+                        final char codificato = ps.scambia(gr.codifica(ps.scambia(letteraF)));
                         final int lampIdx = codificato - 'A';
                         for (int l = 0; l < 26; l++) {
                             if (l == lampIdx) {
@@ -181,6 +243,7 @@ public class EnigmaController {
         inizializzaChoiceBox();
         inizializzaLuci();
         inizializzaTastiera();
+        inizializzaCavi();
         buttons[0].requestFocus(); // Ensure the first button has focus initially
         gridTastiera.requestFocus(); // Ensure the keyboard grid has focus initially
     }
